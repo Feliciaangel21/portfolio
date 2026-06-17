81a412917c50558c86021bab093d41e5d35506af
Feliciaangel21 <80408610+Feliciaangel21@users.noreply.github.com>
2026-06-17 17:22:48 +0900

    About.jsx 업데이트

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -240,7 +240,7 @@ const AboutPage = () => {
       </div>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1RlxbIpEi59MrSZo6-nSumNIETJv1dvaT/view?usp=drive_link" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1B-03MTwRdhb4s2yktisnJUwnebpu_ttZ/view?usp=drivesdk" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
